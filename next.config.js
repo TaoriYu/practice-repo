b3185927e327f818a573c1bec65713abd1b1cacb
Aleksandr Kuznetsov <d100@me.com>
2018-12-29 20:45:18 +0300

    WIP Next deployment

--- next.config.js
+++ next.config.js
@@ -1,44 +1,62 @@
-const webpack = require('webpack');
-const withTypescript = require('./utils/build/withTypescript');
-const withCSS = require('@zeit/next-css');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const getConfig = require('./config/compileConfig');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants'); // Get values from `next-server` package when building on now v
 
-const customs = () => ({
-  webpack: (config, options) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-      path: 'empty'
-    };
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {};
+  }
 
-    if (options.isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin());
-      config.plugins = config.plugins.concat([
-        [/next\/asset/, 'next-server/asset'],
-        [/next\/dynamic/, 'next-server/dynamic'],
-        [/next\/constants/, 'next-server/constants'],
-        [/next\/config/, 'next-server/config'],
-        [/next\/head/, 'next-server/head'],
-      ].map(args => new webpack.NormalModuleReplacementPlugin(...args)));
-    }
+  const withTypescript = require('./utils/build/withTypescript');
+  const withCSS = require('@zeit/next-css');
 
-    if (!options.isServer) {
-      if (process.env.NODE_ANALYZE) {
-        config.plugins.push(new BundleAnalyzerPlugin());
+  return withTypescript(withCSS({
+    cssModules: 'global',
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[name]_[local]__[hash:base64:5]",
+    },
+    ...customs(phase),
+  }));
+};
+
+function customs(phase) {
+  const webpack = require('webpack');
+  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+  const getConfig = require('./config/compileConfig');
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+  return {
+    webpack: (config, options) => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty',
+        path: 'empty'
+      };
+      if (options.isServer) {
+        if (process.env.NODE_ENV !== 'production') {
+          config.plugins.push(new ForkTsCheckerWebpackPlugin());
+        }
+        config.plugins = config.plugins.concat([
+          [/next\/asset/, 'next-server/asset'],
+          [/next\/dynamic/, 'next-server/dynamic'],
+          [/next\/constants/, 'next-server/constants'],
+          [/next\/config/, 'next-server/config'],
+          [/next\/head/, 'next-server/head'],
+        ].map(args => new webpack.NormalModuleReplacementPlugin(...args)));
       }
-    }
-    return config
-  },
-  ...getConfig(),
-});
 
-module.exports = withTypescript(withCSS({
-  cssModules: 'global',
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[name]_[local]__[hash:base64:5]",
-  },
-  ...customs(),
-}));
+      if (!options.isServer) {
+        if (process.env.NODE_ANALYZE) {
+          config.plugins.push(new BundleAnalyzerPlugin());
+        }
+      }
+      return config
+    },
+    ...getConfig(),
+  };
+}
+
