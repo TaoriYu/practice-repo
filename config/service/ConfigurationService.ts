fca06881ce8d15b9c028c6a7e49d01fa5810d1ee
Akuznetsov <kuznetsov_as@tass.ru>
2019-01-10 14:08:40 +0300

    docs(configs): docs for configuration service and adapters

--- config/service/ConfigurationService.ts
+++ config/service/ConfigurationService.ts
@@ -9,6 +9,10 @@ interface IAdapterHandler {
   adapter: IConfigurationAdapter;
 }
 
+/**
+ * Service that expose access for application configuration, it could update configs from remote destination
+ * by using connected adapters
+ */
 export class ConfigurationService implements IConfig<IConfigFields> {
   public _publicRuntimeConfig: IConfigFields;
   public _serverRuntimeConfig: IConfigFields;
@@ -19,20 +23,38 @@ export class ConfigurationService implements IConfig<IConfigFields> {
     this._publicRuntimeConfig = publicRuntimeConfig;
   }
 
+  /**
+   * Returns public configuration you can access it both on server and client.
+   * @returns {IConfigFields}
+   */
   public get publicRuntimeConfig(): IConfigFields {
     return this._publicRuntimeConfig;
   }
 
+  /**
+   * Returns private server configuration, you can access it only from server, on client it will return empty object
+   * @returns {IConfigFields}
+   */
   public get serverRuntimeConfig(): IConfigFields {
     return process.env.IS_SERVER ? this._serverRuntimeConfig : {} as IConfigFields;
   }
 
+  /**
+   * Register a new adapter, adapters used for fetching and retrieving configuration or a part of configuration;
+   * @param {IConfigurationAdapter} adapter
+   * @param {TAdapterPriorities} priority - adapter priority 0 is highest
+   */
   public registerAdapter(adapter: IConfigurationAdapter, priority: TAdapterPriorities): void {
     this.adapters = this.adapters
       .concat([{ adapter, priority }])
       .sort((current, next) => current.priority < next.priority ? 1 : -1);
   }
 
+  /**
+   * Receive all data from the adapters sequent from lover priority to highest priority, higher priority adapters
+   * erase configuration from lower priority adapters
+   * @returns {Promise<void>}
+   */
   public async update() {
     const configurationToFill = this.getFullConfig();
 
--- config/service/adapters/LocalAdapter.ts
+++ config/service/adapters/LocalAdapter.ts
@@ -2,6 +2,9 @@ import { appConfig } from '../../config';
 import { IConfig, IConfigFields } from '../../types/IConfig';
 import { IConfigurationAdapter } from './interfaces';
 
+/**
+ * Simple, mostly example adapter that might return constant data;
+ */
 export class LocalAdapter implements IConfigurationAdapter {
   public get(): IConfig<IConfigFields> | Promise<IConfig<IConfigFields>> | undefined {
     return appConfig;
