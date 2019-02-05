60204c72f760bbcd04b7fd17d85c1af3d59ab13a
Кузнецов Александр Сергеевич <kuznetsov_as@tass.ru>
2019-02-06 00:22:13 +0300

    Revert "feat(core): delivery service"
    This reverts commit ed44b262d88a8f8af8b54bccda2164477df19e86.

--- core/provider/index.ts
+++ core/provider/index.ts
@@ -2,4 +2,3 @@ export * from './InjectStore';
 export * from './makeStore';
 export * from './provide';
 export * from './provideSingleton';
-export * from './withServerAction';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,8 @@
 import 'reflect-metadata';
 import { NextComponentType, NextContext } from 'next';
 import * as React from 'react';
-import App, { AppProps, Container } from 'next/app';
+import App, { Container } from 'next/app';
 import { RuntimeSettings } from '../core/config';
-import { Delivery } from '../core/delivery';
 import { container } from '../di/container';
 
 interface IInitialPropsArgs {
@@ -13,40 +12,21 @@ interface IInitialPropsArgs {
   Component: NextComponentType;
 }
 
-interface IDerivedProps {
-  [ n: string ]: object;
-  derivedProps: Array<{ name: string; derivedProps: any }>;
-}
-
-type TAppProps = AppProps & { pageProps: IDerivedProps };
-
 // tslint:disable-next-line:no-default-export
 export default class CustomApp extends App {
 
   public static async getInitialProps({ Component, ctx }: IInitialPropsArgs) {
-    let pageProps: IDerivedProps = { derivedProps: [] };
+    let pageProps = {};
     const runtimeSettings = container.get(RuntimeSettings);
-
     if (!runtimeSettings.isRuntimeEnabled) {
       await runtimeSettings.enableRuntime();
     }
-    pageProps.derivedProps = await container.get(Delivery).execute(ctx);
 
     if (Component.getInitialProps) {
-      pageProps = Object.assign({}, pageProps, await Component.getInitialProps(ctx));
+      pageProps = await Component.getInitialProps(ctx);
     }
 
     return { pageProps };
-  }
-
-  public constructor(props: TAppProps ) {
-    super(props);
-    if (props.pageProps && props.pageProps.derivedProps) {
-      const delivery = container.get<Delivery>(Delivery);
-      props.pageProps.derivedProps.forEach(({ derivedProps, name }) => {
-        delivery.bindDerived(derivedProps, name);
-      });
-    }
   }
 
   public render() {
