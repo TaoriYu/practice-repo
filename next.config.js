0d2b9993260c31394a996d25bcebb6d5b14c500b
Кузнецов Александр Сергеевич <kuznetsov_as@tass.ru>
2019-03-26 17:46:29 +0300

    feat(core): обновить nextjs до версии 8

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {} // We're never in "production server" phase when in development mode
-    : !process.env.NOW_REGION
-    ? require('next/constants') // Get values from `next` package when building locally
-    : require('next-server/constants'); // Get values from `next-server` package when building on now v
-
-module.exports = (phase) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    return {};
-  }
-
+module.exports = () => {
   const withTypescript = require('./core/build/withTypescript');
   // less also could compile css;
   const withLESS = require('./core/build/withLess');
