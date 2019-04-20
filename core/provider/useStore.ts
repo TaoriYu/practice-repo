731927dc8650d886e6e4d41f40f3fe048e148756
Кузнецов Александр Сергеевич <kuznetsov_as@tass.ru>
2019-04-20 21:02:36 +0300

    build(core): удалить старый useStore (оставив поддержку)

--- core/provider/useStore.ts
+++ core/provider/useStore.ts
@@ -1,7 +1,2 @@
-import { interfaces } from 'inversify';
-import { container } from './container';
-import Newable = interfaces.Newable;
-
-export function useStore<T extends Newable<InstanceType<T>>>(storeName: T): InstanceType<T> {
-  return container.get(storeName);
-}
+// For legacy code support (template before v0.3)
+export { useStore } from './StoreContext';
