4b72f44ee7ea4b274cdd5544aea2c7ae8f4cb12e
Olga Klimonova <taoriyu@gmail.com>
2019-07-13 00:48:56 +0300

    feat(configs): добавить урлы unsplash в конфиги
    -meta: usplash-api

--- config/internalApi/defaultApi.ts
+++ config/internalApi/defaultApi.ts
@@ -3,12 +3,12 @@ import { IWithEnv } from '../../core/config';
 
 export const defaultApi: IWithEnv<IApi> = {
   dev: {
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://api.unsplash.com',
     timeout: 2000,
     public: false,
   },
   production: {
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://api.unsplash.com',
     timeout: 2000,
     public: false,
   },
