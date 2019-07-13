efbd420f12b89974d5541e4cd2179a89a72bb927
Olga Klimonova <taoriyu@gmail.com>
2019-07-13 22:49:50 +0300

    feat(product): render array of photos and fix styles
    -meta: cards-list-page

--- components/IndexPageLayout/IndexPageLayout.tsx
+++ components/IndexPageLayout/IndexPageLayout.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { useStore } from '../../core/provider/useStore';
 import { PhotosStore } from '../../stores/photos';
+import { assignKey } from '../../utils/fn';
 import { ImageCard } from './ImageCard';
+import { intersperse } from 'ramda';
 import s from './indexPageLayout.less';
 
 export function IndexPageLayout() {
@@ -9,7 +11,7 @@ export function IndexPageLayout() {
 
   return (
     <section role="list of photos section" className={s.photosList}>
-      <ImageCard {...photosStore.photos[0]} />
+      {assignKey(intersperse(<div className={s.break} />, photosStore.photos.map(ImageCard)))}
     </section>
   );
 }
