fe57d7ad0818cca07d3b114a335b612a96f4175e
Olga Klimonova <taoriyu@gmail.com>
2019-07-13 21:54:17 +0300

    feat(components): add some creepy styles on card
    -meta: cards-list-page

--- components/IndexPageLayout/ImageCard/ImageCard.tsx
+++ components/IndexPageLayout/ImageCard/ImageCard.tsx
@@ -7,15 +7,15 @@ export function ImageCard({ user, likes, urls, altDescription }: IImageCard) {
     <li className={s.card}>
       <div className={s.content}>
         <div className={s.info}>
-          <span>{`${user.name} aka ${user.username}`}</span>
+          <span className={s.name}>{`${user.name} aka ${user.username}`}</span>
           <div className={s.likes}>
             <div style={{ width: 16, height: 16, backgroundColor: 'grey' }} />
             <span>{likes}</span>
           </div>
         </div>
 
-        <div className={s.image}>
-          <img src={urls.thumb} alt={altDescription} />
+        <div className={s.imageBlock}>
+          <img src={urls.small} alt={altDescription} className={s.image} />
         </div>
       </div>
     </li>
