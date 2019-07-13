1a8df82084ec52063180c03986b04c9c8f8589be
Olga Klimonova <taoriyu@gmail.com>
2019-07-13 17:19:39 +0300

    feat(components): build blueprint of card
    -meta: cards-list-page

--- components/IndexPageLayout/ImageCard/ImageCard.tsx
+++ components/IndexPageLayout/ImageCard/ImageCard.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
 import { IImageCard } from './imageCard.interdace';
-import * as styles from './imageCard.less';
+import s from './imageCard.less';
 
-export function ImageCard({}: IImageCard) {
+export function ImageCard({ user, likes, urls, altDescription }: IImageCard) {
   return (
-    <li className={styles.card} />
+    <li className={s.card}>
+      <div className={s.content}>
+        <div className={s.info}>
+          <span>{`${user.name} aka ${user.username}`}</span>
+          <div className={s.likes}>
+            <div style={{ width: 16, height: 16, backgroundColor: 'grey' }} />
+            <span>{likes}</span>
+          </div>
+        </div>
+
+        <div className={s.image}>
+          <img src={urls.thumb} alt={altDescription} />
+        </div>
+      </div>
+    </li>
   );
 }
