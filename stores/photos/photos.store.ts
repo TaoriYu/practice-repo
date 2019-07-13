e3c526724e06621fbecd268641fcbc52ae6c48eb
Olga Klimonova <taoriyu@gmail.com>
2019-07-13 23:24:37 +0300

    feat(product): prepare store methods to paginate
    -meta: pagination

--- stores/photos/photos.store.ts
+++ stores/photos/photos.store.ts
@@ -1,4 +1,4 @@
-import { observable } from 'mobx';
+import { observable, reaction } from 'mobx';
 import { OApiFactory } from '../../core/api';
 import { makeStore } from '../../core/provider';
 import { accessKey } from '../../secret';
@@ -8,12 +8,30 @@ import { PhotoDto } from './photo.dto';
 export class PhotosStore {
   /** photos from unsplash */
   @observable public photos: PhotoDto[] = [];
+  /** number of pages of data */
+  @observable public pages: number = 1;
 
   public readonly getPhotos = OApiFactory<PhotoDto[]>({
     endpoint: '/photos',
-    params: { page: () => 1 },
+    params: { page: () => this.pages },
     headers: { Authorization: () => `Client-ID ${accessKey}` },
     dto: PhotoDto,
     setter: (photos) => this.photos = photos,
   });
+
+  public readonly getNextPage = OApiFactory<PhotoDto[]>({
+    endpoint: '/photos',
+    params: { page: () => this.pages },
+    headers: { Authorization: () => `Client-ID ${accessKey}` },
+    dto: PhotoDto,
+    setter: (photos) => this.photos = this.photos.concat(photos),
+  });
+
+  public constructor() {
+    reaction(() => this.pages, () => this.getNextPage.observe());
+  }
+
+  public IncrementPages() {
+    this.pages = this.pages + 1;
+  }
 }
