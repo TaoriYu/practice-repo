326d5b50bfed81a880f907fb9f3bd7998a0e1960
Olga Klimonova <taoriyu@gmail.com>
2019-07-16 22:56:49 +0300

    feat(components): fix styles, clear up
    -meta: pagination

--- components/IndexPageLayout/ImageCard/ImageCard.tsx
+++ components/IndexPageLayout/ImageCard/ImageCard.tsx
@@ -5,11 +5,12 @@ import s from './imageCard.less';
 import { Likes } from './Likes';
 import { UserInfo } from './UserInfo';
 import { range } from 'ramda';
+import classNames from 'classnames';
 
 const threshold: number[] = range(0.01, 100).map((_n, i) => i / 100);
 
 export function ImageCard({ user, likes, urls, altDescription }: IImageCard) {
-  const [visible, setVisibility] = React.useState(true);
+  const [visible, setVisibility] = React.useState(false);
   const [ref, registerCallback] =
     useIntersectionObserver<HTMLLIElement>({ threshold, rootMargin: '100px' });
   registerCallback((entries: IntersectionObserverEntry[]) => {
@@ -19,20 +20,22 @@ export function ImageCard({ user, likes, urls, altDescription }: IImageCard) {
   });
 
   return (
-    <li className={s.card} ref={ref}>
+    <li className={classNames(s.listItem, !visible && s.fakeHeight)} ref={ref}>
       {visible &&
-        <div className={s.content}>
-          <div className={s.info}>
-            <UserInfo
-              name={user.name}
-              username={user.username}
-              avatarUrl={user.profileImage && user.profileImage.medium}
-            />
-            <Likes likes={likes} />
-          </div>
+        <div className={s.card}>
+          <div className={s.content}>
+            <div className={s.info}>
+              <UserInfo
+                name={user.name}
+                username={user.username}
+                avatarUrl={user.profileImage && user.profileImage.medium}
+              />
+              <Likes likes={likes} />
+            </div>
 
-          <div className={s.imageBlock}>
-            <img src={urls.small} alt={altDescription} className={s.image} />
+            <div className={s.imageBlock}>
+              <img srcSet={urls.thumb} alt={altDescription} className={s.image} />
+            </div>
           </div>
         </div>
       }
