72f51a2647296f05dfabb3ac94eeb24bf8b20341
Olga Klimonova <taoriyu@gmail.com>
2019-09-10 15:01:24 +0300

    feat(components): add lazy loading mechanism
    - meta: lazy-loading

--- components/shared/Image/Image.tsx
+++ components/shared/Image/Image.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useIntersectionObserver } from '../../../utils/reactHooks/useIntersectionObserver';
 import * as styles from './image.less';
 
 /**
@@ -14,6 +15,13 @@ interface ISources {
   media: string;
 }
 
+interface IImageState {
+  /** src для загрузки дефолтной картинки */
+  defaultSrcState: string;
+  /** sources for downloading photos based on different media */
+  sourcesState?: ISources[];
+}
+
 interface IImageProps {
   /** src для загрузки дефолтной картинки */
   defaultSrc: string;
@@ -21,13 +29,34 @@ interface IImageProps {
   alt?: string;
   /** sources for downloading photos based on different media */
   sources?: ISources[];
+  /** trigger lazy loading for photos */
+  lazy?: boolean;
 }
 
-export function Image({ defaultSrc, alt, sources }: IImageProps) {
+export function Image({ defaultSrc, alt, sources, lazy = false }: IImageProps) {
+  /** default state for Image should initialize with props */
+  const state: IImageState = { defaultSrcState: defaultSrc, sourcesState: sources };
+  /** when lazy prop is set, state should initialize with props */
+  const lazyState: IImageState = { defaultSrcState: '', sourcesState: []};
+  /** settings for intersection observer */
+  const intersectionSettings = { rootMargin: '50px 0px 0px 0px', threshold: 0.1 };
+  const [imageState, setImageState] = React.useState<IImageState>(lazy ? lazyState : state);
+  const [ref, registerCallback] = useIntersectionObserver<HTMLImageElement>(intersectionSettings);
+
+  if (lazy) {
+    registerCallback((entries) => {
+      if (entries[0].isIntersecting) {
+        setImageState({ sourcesState: sources, defaultSrcState: defaultSrc });
+      }
+    });
+  }
+
   return (
     <picture>
-      {sources && sources.map(({ media, src }) => <source src={src} media={media} key={media} />)}
-      <img src={defaultSrc} alt={alt} className={styles.image} />
+      {imageState.sourcesState && imageState.sourcesState.map(({ media, src }) =>
+        <source src={src} media={media} key={media} />,
+      )}
+      <img ref={ref} src={imageState.defaultSrcState} alt={alt} className={styles.image} />
     </picture>
   );
 }
